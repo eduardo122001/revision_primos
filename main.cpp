35573949f1f57a705c11d2212233688a563cb752
eduardo122001 <eduardo.cruz@ucsp.edu.pe>
2022-05-31 19:31:51 -0500

    adding files

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 
 using namespace std;
 
-int random_int(int s,int n){
-  int n1= rand() % n+s;
-  return n1;
+long long random(long long  s,long long n){
+    long long num=s+rand()%((n+1)-s);
+
+  return num;
 }
 
 
 
-int exp_mod(int a,int x,int n){
-    int c=a%n;
-    int r=1;
+long long exp_mod(long long a,long long x,long long n){
+    long long c=a%n;
+    long long r=1;
     while(x>0){
         if(x%2!=0){r=(r*c)%n;}
         c=(c*c)%n;
@@ -21,14 +22,14 @@ int exp_mod(int a,int x,int n){
     return r;
 }
 
-bool es_compuesto(int a,int n,int t,int u){
-    int x=exp_mod(a,u,n);
+bool es_compuesto(long long a,long long n,long long t,long long u){
+    long long x=exp_mod(a,u,n);
     if(x==1||x==n-1){
         return false;
     }
-    for(int i=1;i<t+1;i++){
-        int ip=pow(2,i);
-        int xi=exp_mod(x,2,n);
+    for(long long i=1;i<t+1;i++){
+        //long long ip=pow(2,i);
+        long long xi=exp_mod(x,2,n);
         x=xi;
         if(xi==n-1){
             return false;
@@ -37,22 +38,48 @@ bool es_compuesto(int a,int n,int t,int u){
     return true;
 }
 
-int miller_rabin(int n , int s){
-    int t=0;
-    int u=n-1;
+long long miller_rabin(long long n , long long s){
+    long long t=0;
+    long long u=n-1;
     while(u%2==0){
         u=u/2;
         t++;
     }
-    for(int j=1;j<s+1;j++){
-        int a=random_int(2,n-1);
+    //cout<<"n="<<n<<" t= "<<t<<" u="<<u<<endl;
+    for(long long j=1;j<s+1;j++){
+        long long a=random(2,n-1);
+        //cout<<a<<endl;
         if(es_compuesto(a,n,t,u)){return false;}
     }
+    //cout<<n<<endl;
     return true;
+
 }
 
 int main()
-{   int x=pow(6,2);
-    x=x%8;
+
+{   cout<<miller_rabin(7,6);
+    /*
+    // esta correcto hasta 3 digitos
+    for(long long i=3;i<1000  ;i++){
+        long long s=sqrt(i);
+
+        if(miller_rabin(i,s)){cout<<i<<endl;}
+
+    }
+    */
+    int o=0;
+    for(long long i=1000;i<10000  ;i++){
+        long long s=sqrt(i);
+
+        if(miller_rabin(i,100000)){
+            o++;
+            cout<<o<<"->";
+            cout<<i<<endl;}
+
+    }
+
+
+
     return 0;
 }
